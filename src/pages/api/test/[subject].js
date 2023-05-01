f61ee0963d39f823801bc1e61c8c1e71d0bbc3b6
Abdo Tolba <mtolba2004@gmail.com>
2023-05-01 06:27:45 +0300

    ENV
    Deleted Credidtionls fiels SECURITY :D
    used .env
    updated vercel

--- src/pages/api/test/[subject].js
+++ src/pages/api/test/[subject].js
@@ -1,17 +1,26 @@
-const path = require("path");
 const { google } = require("googleapis");
 
 export default async function handler(req, res) {
   // calculate time
   const start = Date.now();
-  const KEYFILEPATH = path.join("src/Data/credentials.json");
 
   const { subject } = req.query;
 
   const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
   const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
+    credentials: {
+      type: process.env.TYPE,
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: process.env.AUTH_URI,
+      token_uri: process.env.TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    },
     scopes: SCOPES,
   });
 
