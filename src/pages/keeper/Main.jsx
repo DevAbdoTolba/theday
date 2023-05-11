b1a9c43ac31202ec5ddc55c5005e20550057a9ae
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-05-11 15:56:17 +0300

    more ?.

--- src/pages/keeper/Main.jsx
+++ src/pages/keeper/Main.jsx
@@ -3,17 +3,17 @@ import Grid from "@mui/material/Grid";
 
 function Main({ search, notes, setNotes }) {
   const handelDelete = (item) => {
-    setNotes(notes.filter((n) => n !== item));
+    setNotes(notes?.filter((n) => n !== item));
   };
   return (
     <Grid sx={{ marginTop: 2 }} container spacing={2} columns={3}>
       {notes
-        .filter((n) => n.title.includes(search) || n.body.includes(search))
+        ?.filter((n) => n?.title?.includes(search) || n?.body?.includes(search))
         .map((item, index) => (
           <Grid key={index} item sm={1} xs={3}>
             <Note
-              title={item.title}
-              body={item.body}
+              title={item?.title}
+              body={item?.body}
               handelDelete={handelDelete}
               item={item}
             />
