5157294561c288d91b9be33e9e9479e68b105e5f
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-05-13 10:03:31 +0300

    youtube guy fix for vercel pwa error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
   },
-  // other configuration options
+};
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
 });
+
+module.exports = withPWA(nextConfig);
