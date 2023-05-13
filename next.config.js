b2416274fba343b229299d2ffc9003b969d7c9a3
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-05-13 09:54:47 +0300

    same fix old problem

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
 
-const nextConfig = withPWA({
-  // reactStrictMode: true,
+module.exports = withPWA({
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
+    register: true,
+    sw: "service-worker.js",
   },
+  // other configuration options
 });
-
-module.exports = nextConfig;
