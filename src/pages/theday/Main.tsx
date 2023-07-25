7059841ecfc4f0f6ed0e2bd57e311388e11e4f8c
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-07-25 10:47:52 +0300

    thedat mainPc mainPhone
    LAST FILE, now the project is all typesciprt :3

--- src/pages/theday/Main.tsx
+++ src/pages/theday/Main.tsx
@@ -3,8 +3,14 @@ import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import MainPc from "./MainPc";
 import MainPhone from "./MainPhone";
+import Image from "next/image";
 
-function Main({ search, currentSemester, setLoading }) {
+interface Props {
+  search: string;
+  currentSemester: number;
+}
+
+function Main({ search, currentSemester }: Props) {
   return (
     <>
       <MainPc search={search} currentSemester={currentSemester} />
@@ -21,10 +27,7 @@ function Main({ search, currentSemester, setLoading }) {
         <Typography variant="h1" sx={{ mt: 30, textAlign: "center" }}>
           {"Coming Soon "}
         </Typography>
-        <img
-          src="https://camo.githubusercontent.com/cc4b7d9efe6dea4ddaf02677caf064823952913af5e10c82b47fc47bab499e4b/68747470733a2f2f6d656469612e74656e6f722e636f6d2f593562647a6a457663464941414141692f6b697474792d63686173652d706978656c2e676966"
-          alt="Coming Soon"
-        />
+        <Image src="/cat.gif" alt="Coming Soon" width={250} height={250} />
       </Box>
     </>
   );
