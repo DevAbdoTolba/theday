15282c0bc2a7bc8f8ee6e087c16765c6e9e25a4b
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-07-28 12:47:56 +0300

    Supporting arabic for key insertion

--- src/components/KeyDialog.tsx
+++ src/components/KeyDialog.tsx
@@ -7,8 +7,8 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import KeyIcon from "@mui/icons-material/VpnKey";
-import { CircularProgress, Snackbar, Alert } from "@mui/material";
+import { CircularProgress, Snackbar, Alert, Typography } from "@mui/material";
+import TranslateRoundedIcon from "@mui/icons-material/TranslateRounded";
 import { useRouter } from "next/router";
 
 type FormDialogProps = {
@@ -16,7 +16,42 @@ type FormDialogProps = {
   setOpen: (open: boolean) => void;
 };
 
+const translations = {
+  en: {
+    dialogTitle: "Key",
+    dialogContent:
+      "To get access to your class materials, please enter the secret key given to you by your class admin or the site ambassador. If you do not have a key, please contact your classmates to make sure they have one.",
+    transcriptKeyLabel: "Transcript Key",
+    cancelButton: "Cancel",
+    submitKeyButton: "Submit Key",
+    confirmClassTitle: "Confirm Class",
+    confirmClassContent: (className: string) => `Is ${className} your class?`,
+    rejectButton: "Reject",
+    confirmButton: "Confirm",
+    storedClassTitle: "Class Already Stored",
+    storedClassContent:
+      "This class is already stored on your device. Do you want to switch to it?",
+  },
+  ar: {
+    dialogTitle: "مفتاح",
+    dialogContent:
+      "لتحصل على أذن الولوج إلى بيانات الدفعة الجامعية الخاصة بك. يرجى إدخال المفتاح السري الذي أعطاه لك مسؤول الدفعة أو المطور لهذا الموقع في دفعتكم الجامعية. إن لم يكن معك مفتاح يرجى التواصل مع زملائك للتأكد من حصولهم على واحد أو التكلم مع أحد المطورين المذكور أسمهم في الأسفل",
+    transcriptKeyLabel: "مفتاج المنهج",
+    cancelButton: "إلغاء",
+    submitKeyButton: "إرسال المفتاح",
+    confirmClassTitle: "تأكيد الدفعة",
+    confirmClassContent: (className: string): string =>
+      `هل ${className} دفعتك؟`,
+    rejectButton: "رفض",
+    confirmButton: "تأكيد",
+    storedClassTitle: "الدفعة مخزنة بالفعل",
+    storedClassContent:
+      "هذه الدفعة مخزنة بالفعل على جهازك. هل تريد التبديل إليها؟",
+  },
+};
+
 export default function FormDialog({ open, setOpen }: FormDialogProps) {
+  const [language, setLanguage] = React.useState<"en" | "ar">("en");
   const [error, setError] = React.useState("");
   const [loading, setLoading] = React.useState(false);
   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
@@ -26,6 +61,14 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
   const [className, setClassName] = React.useState({ _id: "", class: "" });
   const [key, setKey] = React.useState("");
   const router = useRouter();
+
+  React.useEffect(() => {
+    // @ts-ignore
+    const userLang = navigator.language || navigator.userLanguage;
+    if (userLang.includes("ar")) {
+      setLanguage("ar");
+    }
+  }, []);
 
   const setCloseKeyDialog = () => {
     setOpen(false);
@@ -54,14 +97,15 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
     const key = formJson.key;
     setKey(key);
 
-    // check if the data is valid and not empty
     if (!key) {
       setError("Key is required");
       return;
     }
-    // the data is in the format of mongo ObjectId
+
     if (!key.match(/^[0-9a-fA-F]{24}$/)) {
-      setError("Key is invalid");
+      setError(
+        language === "en" ? "Key is invalid" : "يرجى إدخال المفتاح بشكل صحيح"
+      );
       return;
     }
     setError("");
@@ -110,6 +154,10 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
     router.push(`/theday/q/${key}`);
   };
 
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "en" ? "ar" : "en"));
+  };
+
   return (
     <React.Fragment>
       <Dialog
@@ -120,15 +168,26 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
           onSubmit: handleFormSubmit,
         }}
       >
-        <DialogTitle>
-          Key <KeyIcon />
+        <DialogTitle
+          sx={{
+            textAlign: language === "en" ? "left" : "right",
+            display: "flex",
+            flexDirection: language === "en" ? "row" : "row-reverse",
+            alignItems: "center",
+          }}
+        >
+          <Typography>{translations[language].dialogTitle}</Typography>
+          <Button
+            onClick={toggleLanguage}
+            startIcon={<TranslateRoundedIcon />}
+            sx={{ marginLeft: "auto", fontWeight: "bold" }}
+          >
+            {language === "en" ? "ع" : "E"}
+          </Button>
         </DialogTitle>
-        <DialogContent>
+        <DialogContent dir={language === "en" ? "ltr" : "rtl"}>
           <DialogContentText>
-            To get access to your class materials, please enter the secret key
-            given to you by your class admin or the site ambassador. If you do
-            not have a key, please contact your classmates to make sure they
-            have one.
+            {translations[language].dialogContent}
           </DialogContentText>
           <TextField
             autoFocus
@@ -136,7 +195,21 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
             margin="dense"
             id="key"
             name="key"
-            label="Transcript Key"
+            sx={{
+              "& .MuiInputLabel-root":
+                language === "ar"
+                  ? {
+                      right: "0px",
+                      transformOrigin: "top right",
+                      "& .MuiFormHelperText-root": {
+                        textAlign: "right",
+                      },
+                    }
+                  : {
+                      textAlign: "left",
+                    },
+            }}
+            label={translations[language].transcriptKeyLabel}
             type="text"
             fullWidth
             variant="standard"
@@ -145,7 +218,9 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setCloseKeyDialog()}>Cancel</Button>
+          <Button onClick={() => setCloseKeyDialog()}>
+            {translations[language].cancelButton}
+          </Button>
           <Button
             disabled={loading}
             type={loading ? "button" : "submit"}
@@ -154,35 +229,88 @@ export default function FormDialog({ open, setOpen }: FormDialogProps) {
               minWidth: "20ch",
             }}
           >
-            {loading ? <CircularProgress /> : "Submit Key"}
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              translations[language].submitKeyButton
+            )}
           </Button>
         </DialogActions>
       </Dialog>
 
       <Dialog open={confirmDialogOpen} onClose={handleConfirmDialogClose}>
-        <DialogTitle>Confirm Class</DialogTitle>
+        <DialogTitle>{translations[language].confirmClassTitle}</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Is {className.class} your class?
+            {translations[language].confirmClassContent(className.class)}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleConfirmDialogClose}>Reject</Button>
-          <Button onClick={handleConfirm}>Confirm</Button>
+          <Button onClick={handleConfirmDialogClose}>
+            {translations[language].rejectButton}
+          </Button>
+          <Button onClick={handleConfirm}>
+            {translations[language].confirmButton}
+          </Button>
         </DialogActions>
       </Dialog>
 
       <Dialog open={storedDialogOpen} onClose={handleStoredDialogClose}>
-        <DialogTitle>Class Already Stored</DialogTitle>
+        <DialogTitle>{translations[language].storedClassTitle}</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            This class is already stored on your device. Do you want to switch
-            to it?
+            {translations[language].storedClassContent}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleStoredDialogClose}>Reject</Button>
-          <Button onClick={handleStoredConfirm}>Confirm</Button>
+          <Button onClick={handleStoredDialogClose}>
+            {translations[language].rejectButton}
+          </Button>
+          <Button onClick={handleStoredConfirm}>
+            {translations[language].confirmButton}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={confirmDialogOpen} onClose={handleConfirmDialogClose}>
+        <DialogTitle>
+          {language === "en" ? "Confirm Class" : "تأكيد الدفعة"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {language === "en"
+              ? `Is ${className.class} your class?`
+              : `هل ${className.class} دفعتك؟`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleConfirmDialogClose}>
+            {language === "en" ? "Reject" : "رفض"}
+          </Button>
+          <Button onClick={handleConfirm}>
+            {language === "en" ? "Confirm" : "تأكيد"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={storedDialogOpen} onClose={handleStoredDialogClose}>
+        <DialogTitle>
+          {language === "en" ? "Class Already Stored" : "الدفعة مخزنة بالفعل"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {language === "en"
+              ? "This class is already stored on your device. Do you want to switch to it?"
+              : "هذه الدفعة مخزنة بالفعل على جهازك. هل تريد التبديل إليها؟"}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleStoredDialogClose}>
+            {language === "en" ? "Reject" : "رفض"}
+          </Button>
+          <Button onClick={handleStoredConfirm}>
+            {language === "en" ? "Confirm" : "تأكيد"}
+          </Button>
         </DialogActions>
       </Dialog>
 
