270dcaa128ae9d00bae25022536bcc52f44bc80f
Abdo Tolba <DevAbdoTolba@gmail.com>
2025-12-18 17:15:16 +0200

    feat: add microsoft clarity tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,8 @@ import Head from "next/head";
 import { TranscriptContextProvider } from "../context/TranscriptContext";
 import IndexedProvider from "../context/IndexedContext";
 
+import Script from 'next/script';
+
 // const useStyles = makeStyles((theme) => ({
 //   "@global": {
 //     html: {
@@ -125,6 +127,17 @@ export default function App({
 
   return (
     <>
+
+<Script id="microsoft-clarity-init" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+        `}
+      </Script>
+
       <Head>
         <title>{"TheDay"}</title>
         {/* <meta name="description" content={description} /> */}
@@ -150,6 +163,9 @@ export default function App({
                     opacity: 0,
                   }}
                 />
+
+                  
+
                 <Component {...pageProps} />
                 <Analytics />
               </ThemeProvider>
