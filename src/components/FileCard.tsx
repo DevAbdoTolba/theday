3bb8445b30a85906299a12aae6a50e01ae455d56
Abdo Tolba <DevAbdoTolba@gmail.com>
2025-12-21 17:52:18 +0200

    alot removed
    removed
    - thumbnail function
    - icons
    - wird box
    - thumbnail code

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -11,29 +11,29 @@ import {
 import { ParsedFile } from '../utils/types';
 import { getYoutubeThumbnail } from '../utils/helpers';
 
-const FileIcon = ({ type }: { type: ParsedFile['type'] }) => {
-  switch (type) {
-    case 'pdf': return <PictureAsPdf color="error" />;
-    case 'folder': return <Folder color="primary" />;
-    case 'image': return <ImageIcon color="secondary" />;
-    case 'youtube': return <YouTube color="error" />;
-    case 'video': return <YouTube color="action" />;
-    case 'doc': return <Article color="primary" />;
-    case 'slide': return <Slideshow color="warning" />;
-    case 'sheet': return <TableChart color="success" />;
-    default: return <InsertDriveFile color="disabled" />;
-  }
-};
+// const FileIcon = ({ type }: { type: ParsedFile['type'] }) => {
+//   switch (type) {
+//     case 'pdf': return <PictureAsPdf color="error" />;
+//     case 'folder': return <Folder color="primary" />;
+//     case 'image': return <ImageIcon color="secondary" />;
+//     case 'youtube': return <YouTube color="error" />;
+//     case 'video': return <YouTube color="action" />;
+//     case 'doc': return <Article color="primary" />;
+//     case 'slide': return <Slideshow color="warning" />;
+//     case 'sheet': return <TableChart color="success" />;
+//     default: return <InsertDriveFile color="disabled" />;
+//   }
+// };
 
 export const FileCard = ({ file }: { file: ParsedFile }) => {
   const theme = useTheme();
   
   // Determine thumbnail source
-  let thumbnail = file.thumbnailUrl;
-  if (file.type === 'youtube') {
-    const ytThumb = getYoutubeThumbnail(file.url);
-    if (ytThumb) thumbnail = ytThumb;
-  }
+  // let thumbnail = file.thumbnailUrl;
+  // if (file.type === 'youtube') {
+  //   const ytThumb = getYoutubeThumbnail(file.url);
+  //   if (ytThumb) thumbnail = ytThumb;
+  // }
 
   return (
     <Card 
@@ -78,7 +78,7 @@ export const FileCard = ({ file }: { file: ParsedFile }) => {
             }}
           >
             <Box sx={{ transform: 'scale(2.5)', opacity: 0.5 }}>
-              <FileIcon type={file.type} />
+              {file.type} 
             </Box>
           </Box>
         {/* )} */}
